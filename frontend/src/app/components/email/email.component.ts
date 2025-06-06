d5d67211fce16dea389988eab117b2dda158df55
Drelaky <Drelaky@gmail.com>
2025-06-06 02:45:28 +0200

    feat: added files but i need reworking (not good version)

--- frontend/src/app/components/email/email.component.ts
+++ frontend/src/app/components/email/email.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe, NgClass, NgStyle } from '@angular/common';
-import { Component, HostListener, OnInit, signal } from '@angular/core';
+import { Component, HostListener, OnInit, Signal, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -25,6 +25,7 @@ import { takeUntil } from 'rxjs';
 import { ApiResponse } from '../../types/api.types';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatRadioModule } from '@angular/material/radio';
+import { EmailService } from '../../services/email/email.service';
 
 @Component({
   selector: 'app-email',
@@ -44,69 +45,13 @@ import { MatRadioModule } from '@angular/material/radio';
   styleUrl: './email.component.scss',
 })
 export class EmailComponent extends WithDestroyObservable(Object) implements OnInit {
-  //TODO TYPE FIXING
-  actions: InboxSidebarType[] = [
-    {
-      title: 'Inbox',
-      content: [
-        {
-          icon: faEnvelope,
-          title: 'Inbox',
-          count: 0,
-          active: true,
-        },
-        {
-          icon: faStar,
-          title: 'Starred',
-          count: 0,
-          active: false,
-        },
-        {
-          icon: faWarning,
-          title: 'Spam',
-          count: 0,
-          active: false,
-        },
-        {
-          icon: faTriangleExclamation,
-          title: 'Important',
-          count: 0,
-          active: false,
-        },
-        {
-          icon: faPaperPlane,
-          title: 'Sent',
-          count: 0,
-          active: false,
-        },
-        {
-          icon: faPen,
-          title: 'Drafts',
-          count: 0,
-          active: false,
-        },
-        {
-          icon: faTrash,
-          title: 'Trash',
-          count: 0,
-          active: false,
-        },
-      ],
-    },
-    {
-      title: 'Labels',
-      content: undefined,
-    },
-  ];
-
-  actionsSignal = signal<InboxSidebarType[]>(this.actions);
+  actionsSignal!: WritableSignal<InboxSidebarType[]>;
   isMobile = false;
 
   faStar = faStar;
   faTrash = freeSolidIcons.faTrash;
   faSearch = freeSolidIcons.faSearch;
   faStarSolid = freeReguarIcons.faStar;
-
   faAngleLeft = freeSolidIcons.faAngleLeft;
   faAngleRight = freeSolidIcons.faAngleRight;
   emailsCount = 0;
@@ -116,43 +61,26 @@ export class EmailComponent extends WithDestroyObservable(Object) implements OnI
 
   selectedMail: InboxMailType | null = null;
 
-  constructor(private readonly router: Router, private readonly apiService: ApiService) {
+  constructor(
+    private readonly router: Router,
+    private readonly apiService: ApiService,
+    private emailService: EmailService
+  ) {
     super();
   }
 
   ngOnInit(): void {
+    this.actionsSignal = this.emailService.actionsSignal;
     this.getMails();
-    this.getActions();
+    this.emailService.getActions();
   }
 
   starredMail(mail: InboxMailType): void {
-    mail.isStared = !mail.isStared;
-
-    this.apiService
-      .mailEdit(mail)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        next: (response: ApiResponse<string, InboxMailType>) => {
-          this.getActions();
-        },
-        error: (error: HttpErrorResponse) => {
-          console.error('Error updating mail:', error);
-        },
-      });
+    this.emailService.starredMail(mail);
   }
 
   selectAction(action: InboxType): void {
-    this.actionsSignal.update((actions) =>
-      actions.map((section) => ({
-        ...section,
-        content: Array.isArray(section.content)
-          ? section.content.map((item) => ({
-              ...item,
-              active: item.title === action.title,
-            }))
-          : section.content,
-      }))
-    );
+    this.emailService.selectAction(action);
   }
 
   selectMail(mailId: string): void {
@@ -180,23 +108,8 @@ export class EmailComponent extends WithDestroyObservable(Object) implements OnI
   }
 
   getActions(): void {
-    this.apiService
-      .getActionData()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        next: (response: ApiResponse<string, InboxSidebarType[]>) => {
-          if (this.actions[0].content && response.result[0]?.content) {
-            response.result[0].content = response.result[0].content.map((item, idx) => ({
-              ...item,
-              icon: (this.actions[0].content as any[])[idx]?.icon ?? item.icon,
-            }));
-          }
-          this.actionsSignal.set(response.result);
-        },
-        error: (error: HttpErrorResponse) => {
-          console.error('Error fetching actions:', error);
-        },
-      });
+    this.emailService.getActions();
+    this.actionsSignal = this.emailService.actionsSignal;
   }
 
   searchEmails(searchTerm: string): void {
@@ -221,7 +134,7 @@ export class EmailComponent extends WithDestroyObservable(Object) implements OnI
 
   selectMailCheckbox(mail: InboxMailType): void {
     if (this.selectedMail && this.selectedMail.id === mail.id) {
-      this.selectedMail = null; // Deselect if the same mail is clicked again
+      this.selectedMail = null;
       console.log('Deselected Mail');
       return;
     }
